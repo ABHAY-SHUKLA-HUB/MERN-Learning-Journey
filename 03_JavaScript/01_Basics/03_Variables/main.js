936a3f868e49fe9de07ca34fa0051bbbd845038e
ABHAY-SHUKLA-HUB <shuklaabhayas0@gmail.com>
2026-08-09 00:46:15 +0530

    .\03_JavaScript 01_Basics 03_Variables

---
+++ 03_JavaScript/01_Basics/03_Variables/main.js
@@ -0,0 +1,21 @@
+// Variables Practice
+
+const name = "Abhay";
+let age = 20;
+
+console.log("Name:", name);
+console.log("Age:", age);
+
+// Reassignment
+
+age = 21;
+
+console.log("Updated Age:", age);
+
+// More variables
+
+const college = "Chandigarh University";
+let semester = 3;
+
+console.log("College:", college);
+console.log("Semester:", semester);
