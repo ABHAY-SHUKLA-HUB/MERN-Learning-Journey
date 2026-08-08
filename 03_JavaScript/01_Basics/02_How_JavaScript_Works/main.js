25614a491e12084e26fe0806a6a66133bcc8050f
ABHAY-SHUKLA-HUB <shuklaabhayas0@gmail.com>
2026-08-09 00:43:45 +0530

    .\03_JavaScript 01_Basics 02_How_JavaScript_Works

---
+++ 03_JavaScript/01_Basics/02_How_JavaScript_Works/main.js
@@ -0,0 +1,13 @@
+// JavaScript Engine Example
+console.log("JavaScript code is being executed.");
+
+let a = 10;
+let b = 20;
+
+console.log("Sum:", a + b);
+
+// Function and Call Stack Example
+function greet() {
+    console.log("Hello from greet function");
+}
+greet();
