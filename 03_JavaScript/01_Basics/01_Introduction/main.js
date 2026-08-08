29558974773bce06a39373f3b34cef575c0414e4
ABHAY-SHUKLA-HUB <shuklaabhayas0@gmail.com>
2026-08-09 00:40:07 +0530

    .\03_JavaScript 01_Basics 01_Introduction

---
+++ 03_JavaScript/01_Basics/01_Introduction/main.js
@@ -0,0 +1,31 @@
+// JavaScript kya hai?
+
+// JavaScript ek high-level, dynamically typed, interpreted/JIT-compiled programming language hai jo mainly programming logic aur interactive applications banane ke liye use hoti hai.
+
+// Simple words mein:
+
+// JavaScript ek programming language hai jisme hum instructions likhte hain aur computer unhe execute karta hai.
+
+// JavaScript ka use:
+// Web Development
+//       ↓
+// Frontend
+// Backend
+//       ↓
+// Node.js
+//       ↓
+// APIs
+//       ↓
+// Desktop Applications
+//       ↓
+// Mobile Applications
+//       ↓
+// Automation
+//       ↓
+// Real-time Applications
+
+console.log("Hello");
+
+console.log("Welcome");
+
+console.log("Learning JavaScript");
