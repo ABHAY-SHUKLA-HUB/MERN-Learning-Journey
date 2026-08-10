72848ecc55dcf86f2c5d1091fd78469051c62d31
ABHAY-SHUKLA-HUB <shuklaabhayas0@gmail.com>
2026-08-10 07:22:12 +0530

    .\03_JavaScript 01_Basics 05_Operators

---
+++ 03_JavaScript/01_Basics/05_Operators/main.js
@@ -0,0 +1,69 @@
+// Arithmetic Operators
+
+let a = 10;
+let b = 3;
+
+console.log("Addition:", a + b);
+console.log("Subtraction:", a - b);
+console.log("Multiplication:", a * b);
+console.log("Division:", a / b);
+console.log("Remainder:", a % b);
+console.log("Power:", a ** b);
+
+
+// Assignment Operators
+
+let x = 10;
+
+x += 5;
+console.log("After +=:", x);
+
+x -= 3;
+console.log("After -=:", x);
+
+x *= 2;
+console.log("After *=:", x);
+
+x /= 2;
+console.log("After /=:", x);
+
+
+// Comparison Operators
+
+console.log(10 > 5);
+console.log(10 < 5);
+console.log(10 >= 10);
+console.log(10 <= 5);
+
+console.log(5 == "5");
+console.log(5 === "5");
+
+
+// Logical Operators
+
+console.log(true && true);
+console.log(true && false);
+
+console.log(true || false);
+console.log(false || false);
+
+console.log(!true);
+console.log(!false);
+
+
+// Increment / Decrement
+
+let count = 10;
+
+count++;
+console.log("Increment:", count);
+
+count--;
+console.log("Decrement:", count);
+
+
+// typeof
+
+console.log(typeof "Abhay");
+console.log(typeof 20);
+console.log(typeof true);
