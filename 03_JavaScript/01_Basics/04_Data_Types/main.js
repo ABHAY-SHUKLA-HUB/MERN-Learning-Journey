82d45cd1e627ccf17c14b30bd3f5981865cd2e8d
ABHAY-SHUKLA-HUB <shuklaabhayas0@gmail.com>
2026-08-10 07:16:55 +0530

    .\03_JavaScript 01_Basics 04_Data_Types

---
+++ 03_JavaScript/01_Basics/04_Data_Types/main.js
@@ -0,0 +1,38 @@
+// String
+let name = "Abhay";
+
+// Number
+let age = 20;
+
+// Boolean
+let isStudent = true;
+
+// Undefined
+let futureAge;
+
+// Null
+let selectedUser = null;
+
+// BigInt
+let bigNumber = 12345678901234567890n;
+
+// Symbol
+let id = Symbol("id");
+
+// Object
+let student = {
+    name: "Abhay",
+    age: 20
+};
+
+
+// Checking Data Types
+
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof isStudent);
+console.log(typeof futureAge);
+console.log(typeof selectedUser);
+console.log(typeof bigNumber);
+console.log(typeof id);
+console.log(typeof student);
