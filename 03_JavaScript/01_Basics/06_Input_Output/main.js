b580b5310f3b763a93ba4bb5547a1a9b3436dd76
ABHAY-SHUKLA-HUB <shuklaabhayas0@gmail.com>
2026-08-10 07:25:47 +0530

    .\03_JavaScript 01_Basics 06_Input_Output

---
+++ 03_JavaScript/01_Basics/06_Input_Output/main.js
@@ -0,0 +1,31 @@
+// Output
+
+console.log("Hello JavaScript");
+
+let name = "Abhay";
+let age = 20;
+
+console.log("Name:", name);
+console.log("Age:", age);
+
+
+// Multiple Values
+
+let a = 10;
+let b = 20;
+
+console.log("A:", a);
+console.log("B:", b);
+
+
+// Processing
+
+let sum = a + b;
+
+console.log("Sum:", sum);
+
+
+// Other Console Methods
+
+console.warn("This is a warning");
+console.error("This is an error");
