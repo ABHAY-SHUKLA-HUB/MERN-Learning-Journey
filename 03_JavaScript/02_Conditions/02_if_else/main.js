0643b0d0da797f01b47ecf042fcda5036084df15
ABHAY-SHUKLA-HUB <shuklaabhayas0@gmail.com>
2026-08-12 23:12:00 +0530

    02_if_else

---
+++ 03_JavaScript/02_Conditions/02_if_else/main.js
@@ -0,0 +1,54 @@
+// Age Check
+
+let age = 20;
+
+if (age >= 18) {
+    console.log("Adult");
+} else {
+    console.log("Minor");
+}
+
+
+// Pass / Fail
+
+let marks = 75;
+
+if (marks >= 40) {
+    console.log("Pass");
+} else {
+    console.log("Fail");
+}
+
+
+// Even / Odd
+
+let number = 15;
+
+if (number % 2 === 0) {
+    console.log("Even");
+} else {
+    console.log("Odd");
+}
+
+
+// Login
+
+let isLoggedIn = false;
+
+if (isLoggedIn) {
+    console.log("Welcome");
+} else {
+    console.log("Please Login");
+}
+
+
+// Entry Check
+
+let userAge = 20;
+let hasId = true;
+
+if (userAge >= 18 && hasId === true) {
+    console.log("Entry Allowed");
+} else {
+    console.log("Entry Denied");
+}
