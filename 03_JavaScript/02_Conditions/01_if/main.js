1d7b17f6092f8fe7f15c59ef56e82ddd6fbfe128
ABHAY-SHUKLA-HUB <shuklaabhayas0@gmail.com>
2026-08-12 23:10:52 +0530

    01_if

---
+++ 03_JavaScript/02_Conditions/01_if/main.js
@@ -0,0 +1,44 @@
+let age = 20;
+
+if (age >= 18) {
+    console.log("Adult");
+}
+
+
+let marks = 80;
+
+if (marks >= 40) {
+    console.log("Pass");
+}
+
+
+let number = 10;
+
+if (number % 2 === 0) {
+    console.log("Even Number");
+}
+
+
+
+let userAge = 20;
+let hasId = true;
+
+if (userAge >= 18 && hasId === true) {
+    console.log("Entry Allowed");
+}
+
+
+
+let username = "Abhay";
+
+if (username === "Abhay") {
+    console.log("Welcome Abhay");
+}
+
+
+
+let name = "Abhay";
+
+if (name) {
+    console.log("Name exists");
+}
