b8f522c6d4b96e13b9fdb105b47c83350c458d73
ABHAY-SHUKLA-HUB <shuklaabhayas0@gmail.com>
2026-08-13 23:48:55 +0530

    05_Ternary_Operator

---
+++ 03_JavaScript/02_Conditions/05_Ternary_Operator/main.js
@@ -0,0 +1,44 @@
+// Age
+
+let age = 20;
+
+let ageResult = age >= 18 ? "Adult" : "Minor";
+
+console.log(ageResult);
+
+
+// Pass / Fail
+
+let marks = 75;
+
+let result = marks >= 40 ? "Pass" : "Fail";
+
+console.log(result);
+
+
+// Even / Odd
+
+let number = 10;
+
+let numberType = number % 2 === 0 ? "Even" : "Odd";
+
+console.log(numberType);
+
+
+// Login
+
+let isLoggedIn = true;
+
+let message = isLoggedIn ? "Welcome" : "Please Login";
+
+console.log(message);
+
+
+// Discount
+
+let amount = 1500;
+
+let discountMessage =
+    amount >= 1000 ? "Discount Applied" : "No Discount";
+
+console.log(discountMessage);
